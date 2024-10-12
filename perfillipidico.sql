b1594317bbe77ee1b4cf84feda6cd9c64a0151ee
LuEnciso21 <luisafernanda192002@hotmail.com>
2024-10-11 23:40:14 -0500

    actualizacionBasedeDatos

--- perfillipidico.sql
+++ perfillipidico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 07-10-2024 a las 07:37:37
+-- Tiempo de generación: 12-10-2024 a las 06:38:23
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
@@ -40,9 +40,21 @@ CREATE TABLE `lipidico` (
 --
 
 INSERT INTO `lipidico` (`Documento_identidad`, `CHOLT`, `HDL`, `LDL`, `TRIG`) VALUES
-('1002480112', 145, 140, 140, 140),
-('1012345678', 140, 34, 180, 78),
-('1012345679', 190, 60, 100, 180);
+('1002480112', 190, 75, 80, 100),
+('1005363859', 220, 55, 145, 170),
+('1012345678', 200, 50, 120, 160),
+('1012345679', 190, 60, 100, 140),
+('1012345680', 220, 45, 130, 170),
+('1012345681', 180, 65, 110, 130),
+('1012345682', 195, 55, 115, 150),
+('1012345683', 205, 50, 125, 160),
+('1012345684', 230, 40, 135, 180),
+('1012345685', 185, 60, 105, 140),
+('1012345686', 235, 38, 145, 200),
+('1012345687', 215, 55, 120, 170),
+('1012345688', 200, 42, 110, 180),
+('1012345689', 220, 55, 145, 170),
+('1052410768', 190, 60, 100, 160);
 
 -- --------------------------------------------------------
 
@@ -65,9 +77,21 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`Consecutivo`, `Documento_identidad`, `Nombres`, `Apellidos`, `Edad`, `Genero`, `EPS`) VALUES
-(1, '1012345678', 'CARLOS JULIAN', 'RAMIREZ TORRES', 45, 'Masculino', 'SURA'),
-(2, '1012345679', 'LAURA', 'GOMEZ SANCHEZ', 30, 'FEMENINO', 'NUEVA EPS'),
-(4, '1002480112', 'lU', 'D', 21, 'Femenino', 'Sanitas');
+(17, '1002480112', 'LUISA FERNANDA', 'ENCISO FRANCO', 22, 'Femenino', 'Sanitas'),
+(18, '1012345678', 'CARLOS JULIAN', 'RAMIREZ TORRES', 45, 'Masculino', 'Sanitas'),
+(19, '1012345679', 'LAURA MARIA', 'GOMEZ SANCHEZ', 30, 'Femenino', 'Nueva EPS'),
+(20, '1012345680', 'JUAN', 'MARTINEZ LOPEZ', 50, 'Masculino', 'SURA'),
+(21, '1012345681', 'MARIA CAMILA', 'RODRIGUEZ PEREZ', 28, 'Femenino', 'Compensar'),
+(22, '1012345682', 'ANDRES', 'CATRILLON MORA', 35, 'Masculino', 'Compensar'),
+(23, '1012345683', 'NATALIA', 'SUAREZ PINEDA', 40, 'Femenino', 'Sanitas'),
+(24, '1012345684', 'JUAN DIEGO', 'HERNANDEZ VERA', 33, 'Masculino', 'Compensar'),
+(25, '1012345685', 'JULIANA', 'VARGAS DUARTE', 29, 'Femenino', 'Nueva EPS'),
+(26, '1012345686', 'SEBASTIAN CAMILO', 'MUÑOZ QUINTERO', 55, 'Masculino', 'SURA'),
+(27, '1012345687', 'DANIELA', 'OSPINA CARDENAS', 48, 'Femenino', 'Sanitas'),
+(28, '1012345688', 'MARIA', 'MEDINA CARO', 22, 'Femenino', 'Nueva EPS'),
+(29, '1012345689', 'JUAN', 'OSPINA OSPINA', 37, 'Otro', 'Compensar'),
+(32, '1052410768', 'JUAN ANDRES', 'CARDENAS MOLINA', 74, 'Masculino', 'Sanitas'),
+(33, '1005363859', 'ANDREA', 'CARDENAS MOLINA', 52, 'Femenino', 'Compensar');
 
 --
 -- Índices para tablas volcadas
@@ -94,7 +118,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `Consecutivo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `Consecutivo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
